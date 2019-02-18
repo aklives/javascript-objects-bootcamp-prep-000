f9dab6cd94eeb5dd81fdf9c30a543c4c3e7ec9de
Adam Kearney <aklives@hotmail.com>
2019-02-18 17:53:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,10 @@
 var playlist = { Blur : 'On My Way To The Club' };
 
 function updatePlaylist(object, artist, song){
-  object[key] = song;
+  object[artist] = song;
   return object;
+}
+
+function removeFromPlaylist(object, artist){
+  delete object.artist;
 }
